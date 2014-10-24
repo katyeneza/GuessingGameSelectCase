3e51b94f5fcae5de665baede4d1a819d623bad13
katyeneza <yenezak@pagosa.k12.co.us>
2014-10-24 09:36:13 -0600

    commit

---
+++ GuessingGameSelectCase/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.lblAnswer = New System.Windows.Forms.Label()
+        Me.btnGuess = New System.Windows.Forms.Button()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Location = New System.Drawing.Point(86, 54)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(155, 13)
+        Me.lblInstructions.TabIndex = 0
+        Me.lblInstructions.Text = "Guess a number between 1-10."
+        '
+        'lblAnswer
+        '
+        Me.lblAnswer.AutoSize = True
+        Me.lblAnswer.Location = New System.Drawing.Point(106, 181)
+        Me.lblAnswer.Name = "lblAnswer"
+        Me.lblAnswer.Size = New System.Drawing.Size(0, 13)
+        Me.lblAnswer.TabIndex = 1
+        '
+        'btnGuess
+        '
+        Me.btnGuess.Location = New System.Drawing.Point(89, 105)
+        Me.btnGuess.Name = "btnGuess"
+        Me.btnGuess.Size = New System.Drawing.Size(60, 26)
+        Me.btnGuess.TabIndex = 2
+        Me.btnGuess.Text = "Guess"
+        Me.btnGuess.UseVisualStyleBackColor = True
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(89, 79)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(115, 20)
+        Me.txtGuess.TabIndex = 3
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Location = New System.Drawing.Point(89, 197)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(75, 31)
+        Me.btnRestart.TabIndex = 4
+        Me.btnRestart.Text = "Restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.btnGuess)
+        Me.Controls.Add(Me.lblAnswer)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblInstructions As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer As System.Windows.Forms.Label
+    Friend WithEvents btnGuess As System.Windows.Forms.Button
+    Friend WithEvents txtGuess As System.Windows.Forms.TextBox
+    Friend WithEvents btnRestart As System.Windows.Forms.Button
+
+End Class
---
+++ GuessingGameSelectCase/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim intSecret As Integer
+
+    Private Sub btnGuess_Click(sender As Object, e As EventArgs) Handles btnGuess.Click
+        Dim intGuess As Integer = Me.txtGuess.Text
+
+        Select Case intGuess
+            Case intSecret
+                Me.lblAnswer.Text = "You got it."
+                Me.btnRestart.Enabled = True
+                Me.btnGuess.Enabled = False
+            Case 1 To intSecret
+                Me.lblAnswer.Text = "Too low."
+            Case intSecret To 10
+                Me.lblAnswer.Text = "Too high."
+            Case Is < 1
+                Me.lblAnswer.Text = "You chose a number lower than 1! Guess again."
+            Case Is > 10
+                Me.lblAnswer.Text = "You chose a number higher than 10! Guess again."
+        End Select
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Set initial random number.
+        Randomize()
+        intSecret = Int((10 - 1 + 1) * Rnd() + 1)
+
+        'Set up interface. 
+        Me.btnRestart.Enabled = False
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        'rerandomize intsecret
+        Randomize()
+        intSecret = Int((10 - 1 + 1) * Rnd() + 1)
+
+        'disable reset and restart 
+        Me.btnRestart.Enabled = False
+        Me.btnGuess.Enabled = True
+        Me.lblAnswer.Text = ""
+        Me.txtGuess.Text = ""
+        'picture box is picWord
+
+    End Sub
+End Class
